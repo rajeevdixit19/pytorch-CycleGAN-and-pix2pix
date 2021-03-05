3965d9d7421c7ea7c37b054cfc4e55accbe0fa72
Rajeev Dixit <rajeevdixit19@gmail.com>
2021-03-04 18:06:57 -0800

    Changed imresize to CV2 as scipy func is deprecated

--- util/visualizer.py
+++ util/visualizer.py
@@ -4,7 +4,7 @@ import ntpath
 import time
 from . import util
 from . import html
-from scipy.misc import imresize
+import cv2
 
 
 # save image to the disk
@@ -22,9 +22,9 @@ def save_images(webpage, visuals, image_path, aspect_ratio=1.0, width=256):
         save_path = os.path.join(image_dir, image_name)
         h, w, _ = im.shape
         if aspect_ratio > 1.0:
-            im = imresize(im, (h, int(w * aspect_ratio)), interp='bicubic')
+            im = cv2.resize(src=im, dsize=(h, int(w * aspect_ratio)), interpolation=cv2.INTER_CUBIC)
         if aspect_ratio < 1.0:
-            im = imresize(im, (int(h / aspect_ratio), w), interp='bicubic')
+            im = cv2.resize(src=im, dsize=(int(h / aspect_ratio), w), interpolation=cv2.INTER_CUBIC)
         util.save_image(im, save_path)
 
         ims.append(image_name)
